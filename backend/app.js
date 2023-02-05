ec0c99cfb28cbd4ac8d63b86f4a49e33c2c63e27
wertyyyy12 <wertyyyy102@gmail.com>
2023-02-05 06:02:01 -0800

    fix mongodb_uri; package.json

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const config = require('./utils/config');
-const middleware = require('./utils/middleware');
-const loginRouter = require('./controllers/login');
-const userRouter = require('./controllers/user');
-const assignmentRouter = require('./controllers/assignment');
-const problemRouter = require('./controllers/problem');
-const coursesRouter = require('./controllers/courses');
-const userCourseRouter = require('./controllers/userCourse');
-mongoose.set('strictQuery', false);
-mongoose.connect(config.MONGODB_URI)
-  .then(() => console.log('connected to mongodb'));
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const userRouter = require("./controllers/user");
+const assignmentRouter = require("./controllers/assignment");
+const problemRouter = require("./controllers/problem");
+const coursesRouter = require("./controllers/courses");
+const userCourseRouter = require("./controllers/userCourse");
 
-app.use(express.json())
-app.use(cors())
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => console.log("connected to mongodb"));
 
-app.use('/api/login', loginRouter);
-app.use('/api/users', userRouter);
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/login", loginRouter);
+app.use("/api/users", userRouter);
 app.use(middleware.tokenExtractor);
-app.use('/api/assignments', assignmentRouter);
-app.use('/api/assignments/problems', problemRouter);
+app.use("/api/assignments", assignmentRouter);
+app.use("/api/assignments/problems", problemRouter);
 app.use(middleware.getCourses);
-app.use('/api/courses', coursesRouter);
+app.use("/api/courses", coursesRouter);
 //app.use('/api/userCourses', coursesRouter);
-app.use('/api/users', userCourseRouter);
-app.listen(3001, () => console.log('listening on http://localhost:3001'))
+app.use("/api/users", userCourseRouter);
+app.listen(process.env.PORT, () =>
+  console.log("listening on port " + process.env.PORT)
+);
